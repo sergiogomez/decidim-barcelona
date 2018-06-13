0c2f527a120a9edadd705076109c8ba738b22c8f
Josep Jaume Rey Peroy <josepjaume@gmail.com>
2018-06-13 09:42:54 +0200

    Fix headers

--- config/initializers/secure_headers.rb
+++ config/initializers/secure_headers.rb
@@ -1,8 +1,8 @@
-if Rails.env.production?
+if true && Rails.env.production?
   SecureHeaders::Configuration.default do |config|
     config.cookies = {
       secure: true, # mark all cookies as "Secure"
-      httponly: true, # mark all cookies as "HttpOnly"
+      #httponly: true, # mark all cookies as "HttpOnly"
       samesite: {
         lax: true # mark all cookies as SameSite=lax
       }
